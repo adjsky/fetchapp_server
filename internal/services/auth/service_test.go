4b6d66fd46057abd81d271f5a7265e42e2476f35
adjsky <lopr1965@gmail.com>
2021-05-04 14:49:02 +1000

    add auth service tests

--- internal/services/auth/service_test.go
+++ internal/services/auth/service_test.go
@@ -1,11 +1,103 @@
 package auth
 
 import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
 	"net/http"
+	"net/http/httptest"
+	"server/config"
 	"testing"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/gin-gonic/gin"
+	// initialize a driver
+	_ "github.com/lib/pq"
 )
+
+func TestHandleLogin(t *testing.T) {
+	cfg, err := config.Get()
+	if err != nil {
+		t.Fatal(err)
+	}
+	db, err := sql.Open("postgres", cfg.DatabaseURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	userEmail := "testingemail@mail.ru"
+	userPassword := "asdasd"
+	userPasswordHash, _ := bcrypt.GenerateFromPassword([]byte(userPassword), bcrypt.DefaultCost)
+	_, _ = db.Exec("INSERT INTO Users(email, password) VALUES($1, $2)", userEmail, userPasswordHash)
+	service := authService{config: cfg, database: db}
+	t.Run("handleLogin should return 400 status code if an invalid body is passed", func(t *testing.T) {
+		writer := httptest.NewRecorder()
+		request, _ := http.NewRequest("POST", "auth/login", nil)
+		ctx, _ := gin.CreateTestContext(writer)
+		ctx.Request = request
+		service.handleLogin(ctx)
+		if writer.Code != http.StatusBadRequest {
+			t.Errorf("expected: 400, got: %v", writer.Code)
+		}
+	})
+	t.Run("handleLogin should return 401 status code if an unregistered email is passed", func(t *testing.T) {
+		reqData, err := json.Marshal(loginRequest{
+			Email:    "unregistered@gmail.com",
+			Password: "123",
+		})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		body := bytes.NewReader(reqData)
+		writer := httptest.NewRecorder()
+		request, _ := http.NewRequest("POST", "auth/login", body)
+		ctx, _ := gin.CreateTestContext(writer)
+		ctx.Request = request
+		service.handleLogin(ctx)
+		if writer.Code != 401 {
+			t.Errorf("expected: 401, got: %v", writer.Code)
+		}
+	})
+	t.Run("handleLogin should return 401 status code if an invalid email/password pair is passed", func(t *testing.T) {
+		reqData, err := json.Marshal(loginRequest{
+			Email:    userEmail,
+			Password: "1",
+		})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		body := bytes.NewReader(reqData)
+		writer := httptest.NewRecorder()
+		request, _ := http.NewRequest("POST", "auth/login", body)
+		ctx, _ := gin.CreateTestContext(writer)
+		ctx.Request = request
+		service.handleLogin(ctx)
+		if writer.Code != 401 {
+			t.Errorf("expected: 401, got: %v", writer.Code)
+		}
+	})
+	t.Run("handleLogin should return 200 status code if a valid email/password pair is passed", func(t *testing.T) {
+		reqData, err := json.Marshal(loginRequest{
+			Email:    userEmail,
+			Password: userPassword,
+		})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		body := bytes.NewReader(reqData)
+		writer := httptest.NewRecorder()
+		request, _ := http.NewRequest("POST", "auth/login", body)
+		ctx, _ := gin.CreateTestContext(writer)
+		ctx.Request = request
+		service.handleLogin(ctx)
+		if writer.Code != 200 {
+			t.Errorf("expected: 200, got: %v", writer.Code)
+		}
+	})
+}
 
 func TestGetToken(t *testing.T) {
 	t.Run("GetToken returns an empty string if a request without an authorization header is provided",
