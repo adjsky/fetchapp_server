f71a95c61bfcbe9b2178340df0d5897bc9eb7992
adjsky <lopr1965@gmail.com>
2021-05-31 15:37:28 +1000

    fix webpack error in init script

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 pip3 install -r ./requirements.txt
 if [[ ${ONLY_API} != "true" ]]; then
   cd internal/frontend || echo "couldn't switch to internal/frontend directory"
+  npm i webpack webpack-cli
   npm run build
 fi
